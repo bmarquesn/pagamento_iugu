28cdba51baa667c9e5bdce20a8e59d083866b743
BRUNONOMAR/Bruno <bmarquesn@gmail.com>
2021-10-22 16:42:51 -0300

    Instruções conectar no Iugu

---
+++ __connect_iugu.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+/** insira a chave do Iugu e reomeie este arquivo tirando os 2 underlines (__) no início do nome do arquivo */
+$iugu = new Iugu\Client("KEYIUGU");
